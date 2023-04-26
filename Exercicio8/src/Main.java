f3701d59e24c8ef103f17d5f6516de1710c694f1
Ana Julia Silverio <ana.silverio@zup.com.br>
2023-04-26 11:47:47 -0300

    Exercicio 8

---
+++ Exercicio8/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int A,B,C,D,E,F;
+        A=5;
+        B=2;
+        C=3;
+        D=4;
+        E=6;
+        F=8;
+
+        if (A==B){
+            //A é igual a B
+        }
+        if (C!=D){
+            //C é diferente de D
+        }
+        if (E>F){
+            //E é maior que f
+        }
+
+    }
+}
