4c9f875c0de2638bddaf55bf2ddd43cf98f3aaac
Ana Julia Silverio <ana.silverio@zup.com.br>
2023-04-26 11:16:18 -0300

    Primeiro exercicio

---
+++ exercicio1/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+    public static void main(String [] args){
+        int nota1 = 6;
+        int nota2 = 7;
+        int nota3 = 8;
+        int nota4 = 9;
+
+        double media = (nota1+nota2+nota3+nota4)/4;
+
+        if (media>=7){
+            //ALUNO APROVADO
+        }
+        else{
+            //ALUNO REPROVADO
+        }
+    }
+}
