c9cf57aea4f4175034974d82eb823957d112cdd2
Ana Julia Silverio <ana.silverio@zup.com.br>
2023-04-26 11:25:38 -0300

    Exercicio 2

---
+++ Exercicio2/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String [] args){
+        double altura;
+        double peso;
+        double imc;
+        altura = 159;
+        peso=70;
+        imc = altura * peso;
+
+        if (imc<18.5){
+            //magreza
+        }
+        else if(18.5<imc && imc<24.9){
+            //normal
+        }
+        else if(25<imc && imc<29.9){
+            //sobrepeso
+        }
+        else if (30<imc && imc<39.9) {
+            //obesdidade
+        }
+        else {
+            //Obesidade grave
+        }
+    }
+}
