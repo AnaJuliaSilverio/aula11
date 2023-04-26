529d3094acbb5d0e32e464551e18b7a037ae59c5
Ana Julia Silverio <ana.silverio@zup.com.br>
2023-04-26 11:40:30 -0300

    Exercicio 6

---
+++ Exercicio 6/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        String time1;
+        String time2;
+        int numeroGolTime1,numeroGolTime2;
+        numeroGolTime2 = 2;
+        numeroGolTime1 =1;
+
+        if (numeroGolTime1>numeroGolTime2){
+            //TIME1 VENCEU
+        }
+        else if (numeroGolTime1<numeroGolTime2) {
+            //TIME2 VENCEU
+        }
+        else{
+            //EMPATE
+        }
+    }
+}
