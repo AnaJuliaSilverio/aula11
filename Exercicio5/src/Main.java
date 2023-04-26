86c7f528ab8f4031a8676b5fa6b94fdafe562907
Ana Julia Silverio <ana.silverio@zup.com.br>
2023-04-26 11:36:30 -0300

    Exercicio 5

---
+++ Exercicio5/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int A;
+        int B;
+        A = 5;
+        B = 6;
+
+        if (A>B){
+            //A É MAIOR
+        }
+        //como os valores,pelo enunciado não são iguais, tem q ser else if não apenas else
+        else if (B>A){
+            //B É MAIOR
+        }
+    }
+}
