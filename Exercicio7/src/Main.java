0f5083ad68a5f2ade1474aec7947a8f638ae3c9a
Ana Julia Silverio <ana.silverio@zup.com.br>
2023-04-26 11:43:14 -0300

    Exercicio 7

---
+++ Exercicio7/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int idade;
+        idade = 15;
+
+        if (idade>=18){
+            //MAIOR DE IDADE
+        }
+        else {
+            //MENOR DE IDADE
+        }
+    }
+}
