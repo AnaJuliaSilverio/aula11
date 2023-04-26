1834e562cf13bf3137403262841feaa5015817c2
Ana Julia Silverio <ana.silverio@zup.com.br>
2023-04-26 11:28:52 -0300

    Exercicio 3

---
+++ Exercicio3/src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int valor;
+        valor = 9;
+        if (valor>10){
+            //É MAIOR QUE 10
+        }
+        else {
+            //NÃO É MAIOR QUE 10
+        }
+        }
+    }
