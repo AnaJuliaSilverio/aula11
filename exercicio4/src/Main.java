df8fb2491902c298a6c381e008276849a27661b7
Ana Julia Silverio <ana.silverio@zup.com.br>
2023-04-26 11:33:00 -0300

    Exercicio 4

---
+++ exercicio4/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+            double base;
+            double altura;
+            double area;
+            base = 10;
+            altura = 15;
+            area = base*altura;
+
+        //System.out.printf("A area é %.2f",area);
+        //A area é = area
+
+        }
+    }
